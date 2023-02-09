9926aacabd867039c673c78182de44098d9fc6fb
Zami16 <46745960+zamzami16@users.noreply.github.com>
2023-02-09 06:54:32 +0000

    Add saldo view

--- RukunKematian.py
+++ RukunKematian.py
@@ -8,7 +8,20 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 
-st.write("**KAS RUKUN KEMATIAN.**")
+st.set_page_config(
+    page_title="Rukun Kematian",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
+
+st.write("# **KAS RUKUN KEMATIAN.**")
 
 # excel_data = "https://docs.google.com/spreadsheets/d/1hdNOhGoTdkIVqioERIcdLlN3NgayhJtUEf-BDYScYsk/edit?usp=share_link";
 
@@ -16,11 +29,25 @@ url = "https://docs.google.com/spreadsheets/d/1hdNOhGoTdkIVqioERIcdLlN3NgayhJtUE
 url_1 = url.replace("/edit#gid=", "/export?format=csv&gid=")
 
 df = pd.read_csv(url_1)
+
+saldo = []
+for i in range(len(df)):
+    masuk = df.iloc[0 : i + 1]["Pemasukan"].sum()
+    keluar = df.iloc[0 : i + 1]["Pengeluaran"].sum()
+    saldo.append(masuk - keluar)
+
+df["Saldo"] = saldo
 # print(df.columns.tolist())
-dff = df[["Tanggal", "Pemasukan", "Pengeluaran", "Keterangan"]]
+dff = df[["Tanggal", "Pemasukan", "Pengeluaran", "Saldo", "Keterangan"]]
 
 # AgGrid
 gb = GridOptionsBuilder.from_dataframe(dff)
+gb.configure_auto_height()
+gb.configure_column(
+    "Tanggal",
+    type=["customDateTimeFormat", "nonEditableColumn"],
+    custom_format_string="yyyy-mm-dd",
+)
 gb.configure_column(
     "Pemasukan",
     type=["numericColumn", "customCurrencyFormat"],
@@ -31,8 +58,20 @@ gb.configure_column(
     type=["numericColumn", "customCurrencyFormat"],
     custom_currency_symbol="Rp",
 )
+gb.configure_column(
+    "Saldo",
+    type=["numericColumn", "customCurrencyFormat"],
+    custom_currency_symbol="Rp",
+)
 
 gridOptions = gb.build()
 
-st.write("### Streamlit AgGrid")
-AgGrid(dff, gridOptions=gridOptions, height=200)
+# st.write("### Streamlit AgGrid")
+AgGrid(dff, gridOptions=gridOptions)
+
+
+# st.write("Created with :purple_heart: by Zami16")
+st.markdown(
+    """<div style="text-align: center"> Created with &#128153; by Zami16 </div>""",
+    unsafe_allow_html=True,
+)
